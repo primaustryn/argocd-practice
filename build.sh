40e0218518dc63a34524a010bf204729aa29507e
primaustryn <bingo13@snu.ac.kr>
2024-01-09 22:33:17 +0900

    remove redundant codes and add notion guide link

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
-# 문자열 치환
-IMAGE_REPOSITORY="dalbamm/240109-app-inference"
-DEPLOYMENT_YAML="deployments.yaml"
 VERSION=$(git rev-parse --short $1)
-# echo $VERSION
+
 docker build . -t app-inference
 docker tag app-inference dalbamm/240109-app-inference:$VERSION
 docker push dalbamm/240109-app-inference:$VERSION
